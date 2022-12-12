9a4cd1631476fb75b05906501a13935e4866ca39
Mason Wilde <masonwilde1@gmail.com>
2022-12-11 20:49:25 -0700

    Add models and initialize database

--- SkiResortStats/resorts/admin.py
+++ SkiResortStats/resorts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ActiveRecord
+
+admin.site.register(ActiveRecord)
--- SkiResortStats/resorts/models.py
+++ SkiResortStats/resorts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class ActiveRecord(models.Model):
+    resort_name = models.CharField(max_length=50)
+    snow_overnight = models.IntegerField(default=0)
+    snow_24hrs = models.IntegerField(default=0)
+    snow_48hrs = models.IntegerField(default=0)
+    snow_72hrs = models.IntegerField(default=0)
+    snow_7days = models.IntegerField(default=0)
+    snow_30days = models.IntegerField(default=0)
+    snow_total = models.IntegerField(default=0)
+    snow_bae_depth = models.IntegerField(default=0)
+    trails_total = models.IntegerField(default=0)
+    trails_open = models.IntegerField(default=0)
+    lifts_total = models.IntegerField(default=0)
+    lifts_open = models.IntegerField(default=0)
