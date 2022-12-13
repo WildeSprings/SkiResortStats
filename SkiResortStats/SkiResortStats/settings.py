6093df57804480de21a55c600117783d15baf800
Mason Wilde <masonwilde1@gmail.com>
2022-12-13 10:51:41 -0700

    Add more environment variables

--- SkiResortStats/SkiResortStats/settings.py
+++ SkiResortStats/SkiResortStats/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'SkiResortStats.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'SkiDB', 
+        'USER': config("DB_USER"), 
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_IP"), 
+        'PORT': '5432',
     }
 }
 
