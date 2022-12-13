2e127636119fbef12468cd2c49ce9ecada2e81f5
Mason Wilde <masonwilde1@gmail.com>
2022-12-13 12:22:05 -0700

    Update index view to show data for resorts

--- SkiResortStats/resorts/models.py
+++ SkiResortStats/resorts/models.py
@@ -10,8 +10,10 @@ class ActiveRecord(models.Model):
     snow_7days = models.IntegerField(default=0)
     snow_30days = models.IntegerField(default=0)
     snow_total = models.IntegerField(default=0)
-    snow_bae_depth = models.IntegerField(default=0)
+    snow_base_depth = models.IntegerField(default=0)
     trails_total = models.IntegerField(default=0)
     trails_open = models.IntegerField(default=0)
     lifts_total = models.IntegerField(default=0)
     lifts_open = models.IntegerField(default=0)
+    def __str__(self):
+        return self.resort_name
--- SkiResortStats/resorts/views.py
+++ SkiResortStats/resorts/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import ActiveRecord
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the resorts index.")
+    resort_list = ActiveRecord.objects.all
+    template = loader.get_template('resorts/index.html')
+    context = {
+        'resort_list': resort_list,
+    }
+    return HttpResponse(template.render(context, request))
