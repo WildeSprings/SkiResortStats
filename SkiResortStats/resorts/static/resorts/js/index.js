36bae3b2c698277c00313690aa1b57ae72013944
Mason Wilde <masonwilde1@gmail.com>
2022-12-15 13:21:02 -0700

    Add horizontal scrolling to table

--- SkiResortStats/resorts/static/resorts/js/index.js
+++ SkiResortStats/resorts/static/resorts/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#resortTable').DataTable( {
+        scrollX: true,
         dom: 'Qlfrtip',
         searchBuilder: {
             columns: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
