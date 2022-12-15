ff0101e9088a478ecc2c2481cda748ae4602fc30
Mason Wilde <masonwilde1@gmail.com>
2022-12-15 09:20:07 -0700

    Add filtering for data table

---
+++ SkiResortStats/resorts/static/resorts/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#resortTable').DataTable( {
+        dom: 'Qlfrtip',
+        searchBuilder: {
+            columns: [1,2,3,4,5,6,7,8,9,10,11,12]
+        }
+    });
+});
