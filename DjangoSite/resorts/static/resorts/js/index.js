871af138a01cbe75d60b041d4d1a9765da98aab6
Mason Wilde <masonwilde1@gmail.com>
2022-12-15 17:16:38 -0700

    Fix table styling

--- DjangoSite/resorts/static/resorts/js/index.js
+++ DjangoSite/resorts/static/resorts/js/index.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     $('#resortTable').DataTable( {
         scrollX: true,
+        scrollCollapse: true,
+        paging: true,
+        'fixedColumns'  :   {
+          leftColumns: 1,
+        },
         dom: 'Qlfrtip',
         searchBuilder: {
             columns: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
