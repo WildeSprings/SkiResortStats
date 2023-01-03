c59e304a5153f19cf6cef9283b114aa35863bb73
Ellis Springe <ellis.springe@ibm.com>
2023-01-03 12:04:51 -0700

    YABF (yet another bug fix)

--- ResortsUpdater/resorts/mtnpowder.py
+++ ResortsUpdater/resorts/mtnpowder.py
@@ -15,19 +15,19 @@ def GetData(RESORT_NAME, COUNTRY, REGION, PASSES, RESORT_NUM, RESERVATION):
 
     snow_report = json_resp["SnowReport"]
     lifts_open = snow_report["TotalOpenLifts"] if snow_report["TotalOpenLifts"] != "--" else None
-    lifts_total = snow_report["TotalLifts"] if snow_report["TotalOpenLifts"] != "--" else None
-    trails_open = snow_report["TotalOpenTrails"] if snow_report["TotalOpenLifts"] != "--" else None
-    trails_total = snow_report["TotalTrails"] if snow_report["TotalOpenLifts"] != "--" else None
+    lifts_total = snow_report["TotalLifts"] if snow_report["TotalLifts"] != "--" else None
+    trails_open = snow_report["TotalOpenTrails"] if snow_report["TotalOpenTrails"] != "--" else None
+    trails_total = snow_report["TotalTrails"] if snow_report["TotalTrails"] != "--" else None
 
-    snow_stats = snow_report["BaseArea"] if snow_report["TotalOpenLifts"] != "--" else None
-    snow_overnight = snow_stats["SinceLiftsClosedIn"] if snow_report["TotalOpenLifts"] != "--" else None
-    snow_24hrs = snow_stats["Last24HoursIn"] if snow_report["TotalOpenLifts"] != "--" else None
-    snow_48hrs = snow_stats["Last48HoursIn"] if snow_report["TotalOpenLifts"] != "--" else None
-    snow_72hrs = snow_stats["Last72HoursIn"] if snow_report["TotalOpenLifts"] != "--" else None
-    snow_7days = snow_stats["Last7DaysIn"] if snow_report["TotalOpenLifts"] != "--" else None
+    snow_stats = snow_report["BaseArea"] if snow_report["BaseArea"] != "--" else None
+    snow_overnight = snow_stats["SinceLiftsClosedIn"] if snow_report["SinceLiftsClosedIn"] != "--" else None
+    snow_24hrs = snow_stats["Last24HoursIn"] if snow_report["Last24HoursIn"] != "--" else None
+    snow_48hrs = snow_stats["Last48HoursIn"] if snow_report["Last48HoursIn"] != "--" else None
+    snow_72hrs = snow_stats["Last72HoursIn"] if snow_report["Last72HoursIn"] != "--" else None
+    snow_7days = snow_stats["Last7DaysIn"] if snow_report["Last7DaysIn"] != "--" else None
     snow_30days = None
-    snow_total = snow_report["SeasonTotalIn"] if snow_report["TotalOpenLifts"] != "--" else None
-    snow_base_depth = snow_stats["BaseIn"] if snow_report["TotalOpenLifts"] != "--" else None
+    snow_total = snow_report["SeasonTotalIn"] if snow_report["SeasonTotalIn"] != "--" else None
+    snow_base_depth = snow_stats["BaseIn"] if snow_report["BaseIn"] != "--" else None
     return resort.ResortActiveRecord(RESORT_NAME, snow_overnight, snow_24hrs,
                                      snow_48hrs, snow_72hrs, None, None,
                                      snow_total, snow_base_depth, lifts_open, lifts_total,
@@ -36,9 +36,8 @@ def GetData(RESORT_NAME, COUNTRY, REGION, PASSES, RESORT_NUM, RESERVATION):
 
 
 
-
-# GetData("Winter Park", "USA", "Colorado", "Ikon", 5),
-# GetData("Stratton", "USA", "New York", "Ikon", 1),
+print(GetData("Winter Park", "USA", "Colorado", "Ikon", 5))
+print(GetData("Stratton", "USA", "New York", "Ikon", 1))
 # GetData("Snowshoe", "USA", "Colorado", "Ikon", 2),
 # GetData("Blue", "Canada", "Ontario", "Ikon", 3),
 # GetData("Tremblant", "Canada", "Quebec", "Ikon", 4),
