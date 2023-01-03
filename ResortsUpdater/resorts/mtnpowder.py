4f5be16bdf96c73a1b91756cf9b3c0fdc79119e6
Ellis Springe <ellis.springe@ibm.com>
2023-01-03 12:10:30 -0700

    YABF

--- ResortsUpdater/resorts/mtnpowder.py
+++ ResortsUpdater/resorts/mtnpowder.py
@@ -20,14 +20,14 @@ def GetData(RESORT_NAME, COUNTRY, REGION, PASSES, RESORT_NUM, RESERVATION):
     trails_total = snow_report["TotalTrails"] if snow_report["TotalTrails"] != "--" else None
 
     snow_stats = snow_report["BaseArea"] if snow_report["BaseArea"] != "--" else None
-    snow_overnight = snow_stats["SinceLiftsClosedIn"] if snow_report["SinceLiftsClosedIn"] != "--" else None
-    snow_24hrs = snow_stats["Last24HoursIn"] if snow_report["Last24HoursIn"] != "--" else None
-    snow_48hrs = snow_stats["Last48HoursIn"] if snow_report["Last48HoursIn"] != "--" else None
-    snow_72hrs = snow_stats["Last72HoursIn"] if snow_report["Last72HoursIn"] != "--" else None
-    snow_7days = snow_stats["Last7DaysIn"] if snow_report["Last7DaysIn"] != "--" else None
+    snow_overnight = snow_stats["SinceLiftsClosedIn"] if snow_stats["SinceLiftsClosedIn"] != "--" else None
+    snow_24hrs = snow_stats["Last24HoursIn"] if snow_stats["Last24HoursIn"] != "--" else None
+    snow_48hrs = snow_stats["Last48HoursIn"] if snow_stats["Last48HoursIn"] != "--" else None
+    snow_72hrs = snow_stats["Last72HoursIn"] if snow_stats["Last72HoursIn"] != "--" else None
+    snow_7days = snow_stats["Last7DaysIn"] if snow_stats["Last7DaysIn"] != "--" else None
     snow_30days = None
     snow_total = snow_report["SeasonTotalIn"] if snow_report["SeasonTotalIn"] != "--" else None
-    snow_base_depth = snow_stats["BaseIn"] if snow_report["BaseIn"] != "--" else None
+    snow_base_depth = snow_stats["BaseIn"] if snow_stats["BaseIn"] != "--" else None
     return resort.ResortActiveRecord(RESORT_NAME, snow_overnight, snow_24hrs,
                                      snow_48hrs, snow_72hrs, None, None,
                                      snow_total, snow_base_depth, lifts_open, lifts_total,
@@ -36,8 +36,8 @@ def GetData(RESORT_NAME, COUNTRY, REGION, PASSES, RESORT_NUM, RESERVATION):
 
 
 
-print(GetData("Winter Park", "USA", "Colorado", "Ikon", 5))
-print(GetData("Stratton", "USA", "New York", "Ikon", 1))
+#print(GetData("Winter Park", "USA", "Colorado", "Ikon", 5))
+#print(GetData("Stratton", "USA", "New York", "Ikon", 1))
 # GetData("Snowshoe", "USA", "Colorado", "Ikon", 2),
 # GetData("Blue", "Canada", "Ontario", "Ikon", 3),
 # GetData("Tremblant", "Canada", "Quebec", "Ikon", 4),
