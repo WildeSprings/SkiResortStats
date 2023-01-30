d30143bc6595ad0d4059cdd462568181d3552d08
Ellis Springe <espringe5@sbcglobal.net>
2023-01-30 15:04:31 -0700

    Add button filtering

--- DjangoSite/resorts/static/resorts/js/index.js
+++ DjangoSite/resorts/static/resorts/js/index.js
@@ -11,4 +11,23 @@ $(document).ready(function() {
             columns: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
         }
     });
+    
+    $('ul').on('click', 'a', function() {
+
+        if ($(this).text() == "Ikon & Colorado") {
+            table
+            .columns(13)
+            .search("Ikon")
+            .columns(14)
+            .search("Colorado")
+            .draw();
+        } else if ($(this).text() == "Epic & Colorado") {
+            table
+            .columns(13)
+            .search("Epic")
+            .columns(14)
+            .search("Colorado")
+            .draw()
+        } 
+    });
 });
